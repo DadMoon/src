15b449ba9b84eecdefd2f468fb1ca010592649ce
DadMoon <120421327+DadMoon@users.noreply.github.com>
2022-12-13 21:59:55 +0100

    Create avatar.js

---
+++ avatar.js
@@ -0,0 +1,6 @@
+{
+    "username": "Alpha Stealer",
+    "href": "None",
+    "avatar_url": "https://cdn.discordapp.com/attachments/1052314761234227200/1052315172015968376/alpha.png",
+    "credits": "DadMoon"
+}
